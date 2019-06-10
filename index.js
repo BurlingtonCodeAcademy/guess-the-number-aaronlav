179031e2018462516401c50b6c2b3b94f0b2edcd
Aaron Lavery <aaroncl55@gmail.com>
2019-06-10 13:00:41 -0400

    hello

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
+var randomNum = Math.floor(Math.random() * 100) + 1;
+var attempts = 5;
+var low = 1;
+var high  = 100;
+console.log(randomNum);
+process.stdin.on('data', (chunk) => { 
+  let num1 = chunk.toString().trim();
 
-function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+
+  if (num1 ==='lower')
+  {
+    high = randomNum - 1;
+    console.log(low + ', ' + high);
+    console.log('You have '+attempts+' attempts remaining.');
+  }
+
+  else if (num1 ==='higher')
+  {
+    low=randomNum + 1;
+    console.log(low + ', ' + high);
+    console.log('You have '+attempts+' attempts remaining.');
+  }
+
+  else if ("correct!")
+  {
+    console.log('Damn I am good.');
+    console.log('I mean, shit, I still had '+ (5-attempts) + ' attempts remaining.');
+    process.exit(0);
+  }
+
+ // else
+ // {
+ //   console.log("Try a different number, dumbass.");
+  //  console.log('You still have '+attempts+' remaining.');
+ // }
+
+  attempts--;
+  if (attempts == -1) //has to be negative to be correct?
+  {
+    console.log('Sorry, but I SUCK.');
+    process.exit(0);
+  }
+ randomNum = Math.floor(Math.random() * 100 - (high - low)) + low
 }
-
-start();
-
-async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
-}
+)
